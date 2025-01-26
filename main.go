6b26b5188919fc80dc24685fc27e0cba7636d411
MrD0511 <sharmadhruv00005@gmail.com>
2025-01-26 22:17:54 +0530

    added kubernates client

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
-	// "fmt"
-	// "os"
-	// "path/filepath"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
 	"github.com/MrD0511/deck/cli"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
 	cli.Cli_main()
+
+	kubeconfig := os.Getenv("KUBECONFIG")
+	if kubeconfig == "" && homedir.HomeDir() != "" {
+		kubeconfig = filepath.Join(homedir.HomeDir(), ".kube", "config")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatalf("Failed to build kubeconfig: %v", err)
+	}
+	
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("Failed to create clientset: %v", err)
+	}
+
+	pods, err := clientset.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Failed to list pods: %v", err)
+	}
+
+	fmt.Println("List of Pods: ")
+	for _, pod := range pods.Items {
+		fmt.Printf("Pod Name: %s, Status: %s\n", pod.Name, pod.Status.Phase)
+	}
+
 }
